853f6bc7f460e5e20f5d62b19c56eb17163701fb
xiaogang <969175225@qq.com>
2025-05-28 00:24:34 +0800

    测试套件增加搜索

--- apps/jk_case/filter_set.py
+++ apps/jk_case/filter_set.py
@@ -1,5 +1,6 @@
 import django_filters
-from jk_case.models import TestCase
+from jk_case.models import TestCase, TestSuite
+from projects.models import Projects
 
 
 class TestCaseFilter(django_filters.FilterSet):
@@ -10,4 +11,15 @@ class TestCaseFilter(django_filters.FilterSet):
         model = TestCase
         fields = ['name']
 
+
+class SuiteFilter(django_filters.FilterSet):
+    """ 定义字段，搜索方式：模糊搜索 """
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    project = django_filters.ModelChoiceFilter(queryset=Projects.objects.all())
+    # 或者使用下面这种方式通过project的name字段过滤
+    # project__name = django_filters.CharFilter(field_name='project__name', lookup_expr='icontains')
+
+    class Meta:
+        model = TestSuite
+        fields = ['name', 'project']
 
--- apps/jk_case/views.py
+++ apps/jk_case/views.py
@@ -2,11 +2,11 @@ from common.utils import APIResponse
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import TestSuite, SuiteCaseRelation, TestExecution, InterFace, TestCase, Module
-from .serializers import (TestSuiteSerializer, SuiteCaseRelationSerializer, TestExecutionSerializer,
+from .models import TestSuite, TestExecution, InterFace, TestCase, Module
+from .serializers import (TestSuiteSerializer, TestExecutionSerializer,
                           InterFaceSerializer, TestCaseSerializer, ModuleSerializer, AllModuleSerializer,
                           InterFaceIdNameSerializer, SimpleTestCaseSerializer)
-from .filter_set import TestCaseFilter
+from .filter_set import TestCaseFilter, SuiteFilter
 from datetime import timezone
 from common.error_codes import ErrorCode
 from common.handle_test.tasks import async_execute_suite
@@ -174,12 +174,13 @@ class TestCaseViewSet(viewsets.ModelViewSet):
         serializer = SimpleTestCaseSerializer(queryset, many=True)
         return APIResponse(data=serializer.data)
 
+
 class TestSuiteViewSet(viewsets.ModelViewSet):
     queryset = TestSuite.objects.all().order_by('-id')
     serializer_class = TestSuiteSerializer
     permission_classes = [permissions.IsAuthenticated]
-    # filterset_fields = ['project', 'name']
-    # search_fields = ['name', 'description']
+    # 自定义模糊搜索 字段
+    filterset_class = SuiteFilter
 
     def perform_create(self, serializer):
         """自动设置创建人"""
@@ -187,141 +188,141 @@ class TestSuiteViewSet(viewsets.ModelViewSet):
 
     def perform_update(self, serializer):
         # 记录修改日志
-        instance = serializer.save(updated_by=self.request.user)
+        serializer.save(updated_by=self.request.user)
 
-    @action(detail=True, methods=['post'], url_path='add-cases')
-    def add_cases(self, request, pk=None):
-        """批量添加用例到测试套件
-        request body example:
-            {"case_ids": [1, 2, 3]}
-        """
-        suite = self.get_object()
-        data = request.data
-
-        # 验证请求数据格式
-        case_ids = data.get('case_ids', [])
-        if not isinstance(case_ids, list):
-            d = {
-                'code': ErrorCode.PARAM_ERROR.code,
-                'message': '请求参数应为用例列表',
-            }
-            return Response(d, status=400)
-
-        # 处理批量创建
-        relations_to_create = []
-        failed_list = []
-
-        for case_id in case_ids:
-            # 验证用例是否存在
-            try:
-                case = TestCase.objects.get(id=case_id)
-                # 检查重复添加
-                if SuiteCaseRelation.objects.filter(suite_id=suite, case_id=case).exists():
-                    failed_list.append(case_id)
-                    continue
-                max_order = SuiteCaseRelation.objects.filter(suite_id=suite)
-                local_order = 0
-                if max_order:
-                    local_order = max_order.values('order').order_by('-order')[0]['order'] + 1
-
-                relations_to_create.append(SuiteCaseRelation(
-                    suite=suite,
-                    case=case,
-                    order=local_order if local_order else 1,
-                ))
-            except TestCase.DoesNotExist:
-                failed_list.append(case_id)
-        # 批量创建关系
-        if relations_to_create:
-            SuiteCaseRelation.objects.bulk_create(relations_to_create)
-
-        # 构建响应
-        response_data = {
-            "success_count": len(relations_to_create),
-            "failed_count": len(failed_list)
-        }
-        return Response(data=response_data)
-
-    @action(detail=True, methods=['get'], url_path='relation-cases')
-    def relation_cases(self, request, pk=None):
-        """获取套件的用例"""
-        suite = self.get_object()
-        # 获取套件关联的用例关系对象
-        relations = SuiteCaseRelation.objects.filter(suite=suite)
-
-        # 使用嵌套序列化器返回详细信息
-        serializer = SuiteCaseRelationSerializer(
-            relations,
-            many=True,
-            context={'request': request}
-        )
-
-        return Response(serializer.data)
-
-    @action(detail=True, methods=['post'], url_path='remove-cases')
-    def remove_cases(self, request, pk=None):
-        """批量解绑测试套件关联的用例
-        request body example:
-            {"case_ids": [1, 2, 3]}
-        """
-        suite = self.get_object()
-        data = request.data
-
-        # 验证请求数据格式
-        case_ids = data.get('case_ids', [])
-        if not isinstance(case_ids, list):
-            data = {
-                'code': ErrorCode.PARAM_ERROR.code,
-                'message': '请求参数应为用例列表',
-            }
-            return Response(data, status=400)
-        # 过滤有效关联记录
-        relations = SuiteCaseRelation.objects.filter(
-            suite=suite,
-            case_id__in=case_ids
-        )
-
-        # 执行删除操作
-        deleted_count, _ = relations.delete()
-
-        # 构建响应
-        response_data = {
-            "success_count": deleted_count,
-            "failed_count": len(case_ids) - deleted_count
-        }
-        return Response(data=response_data)
-
-    @action(detail=True, methods=['post'], url_path='reorder-cases')
-    def reorder_cases(self, request, pk=None):
-        """调整用例顺序
-        request body example:
-            [{"case_id": 1, "order": 1}, {"case_id": 2, "order": 2}]
-        """
-        suite = self.get_object()
-        data = request.data
-        relations_to_update = []
-        for case_info in data:
-            try:
-                case = TestCase.objects.get(id=case_info['case_id'])
-                relation_record_id = SuiteCaseRelation.objects.get(suite_id=suite, case_id=case).id
-                relations_to_update.append(
-                    SuiteCaseRelation(
-                        id=relation_record_id,
-                        order=case_info['order'],
-                    )
-                )
-            except TestCase.DoesNotExist:
-                return Response({
-                    'code': ErrorCode.PARAM_ERROR.code,
-                    'message': f"用例 {case_info['case_id']} 不存在",
-                }, status=400)
-            except SuiteCaseRelation.DoesNotExist:
-                return Response({
-                    'code': ErrorCode.PARAM_ERROR.code,
-                    'message': f"用例 {case_info['case_id']} 不在套件 {suite.name} 中",
-                }, status=400)
-        SuiteCaseRelation.objects.bulk_update(relations_to_update, ['order'])
-        return Response({'status': 'success'})
+    # @action(detail=True, methods=['post'], url_path='add-cases')
+    # def add_cases(self, request, pk=None):
+    #     """批量添加用例到测试套件
+    #     request body example:
+    #         {"case_ids": [1, 2, 3]}
+    #     """
+    #     suite = self.get_object()
+    #     data = request.data
+    #
+    #     # 验证请求数据格式
+    #     case_ids = data.get('case_ids', [])
+    #     if not isinstance(case_ids, list):
+    #         d = {
+    #             'code': ErrorCode.PARAM_ERROR.code,
+    #             'message': '请求参数应为用例列表',
+    #         }
+    #         return Response(d, status=400)
+    #
+    #     # 处理批量创建
+    #     relations_to_create = []
+    #     failed_list = []
+    #
+    #     for case_id in case_ids:
+    #         # 验证用例是否存在
+    #         try:
+    #             case = TestCase.objects.get(id=case_id)
+    #             # 检查重复添加
+    #             if SuiteCaseRelation.objects.filter(suite_id=suite, case_id=case).exists():
+    #                 failed_list.append(case_id)
+    #                 continue
+    #             max_order = SuiteCaseRelation.objects.filter(suite_id=suite)
+    #             local_order = 0
+    #             if max_order:
+    #                 local_order = max_order.values('order').order_by('-order')[0]['order'] + 1
+    #
+    #             relations_to_create.append(SuiteCaseRelation(
+    #                 suite=suite,
+    #                 case=case,
+    #                 order=local_order if local_order else 1,
+    #             ))
+    #         except TestCase.DoesNotExist:
+    #             failed_list.append(case_id)
+    #     # 批量创建关系
+    #     if relations_to_create:
+    #         SuiteCaseRelation.objects.bulk_create(relations_to_create)
+    #
+    #     # 构建响应
+    #     response_data = {
+    #         "success_count": len(relations_to_create),
+    #         "failed_count": len(failed_list)
+    #     }
+    #     return Response(data=response_data)
+    #
+    # @action(detail=True, methods=['get'], url_path='relation-cases')
+    # def relation_cases(self, request, pk=None):
+    #     """获取套件的用例"""
+    #     suite = self.get_object()
+    #     # 获取套件关联的用例关系对象
+    #     relations = SuiteCaseRelation.objects.filter(suite=suite)
+    #
+    #     # 使用嵌套序列化器返回详细信息
+    #     serializer = SuiteCaseRelationSerializer(
+    #         relations,
+    #         many=True,
+    #         context={'request': request}
+    #     )
+    #
+    #     return Response(serializer.data)
+    #
+    # @action(detail=True, methods=['post'], url_path='remove-cases')
+    # def remove_cases(self, request, pk=None):
+    #     """批量解绑测试套件关联的用例
+    #     request body example:
+    #         {"case_ids": [1, 2, 3]}
+    #     """
+    #     suite = self.get_object()
+    #     data = request.data
+    #
+    #     # 验证请求数据格式
+    #     case_ids = data.get('case_ids', [])
+    #     if not isinstance(case_ids, list):
+    #         data = {
+    #             'code': ErrorCode.PARAM_ERROR.code,
+    #             'message': '请求参数应为用例列表',
+    #         }
+    #         return Response(data, status=400)
+    #     # 过滤有效关联记录
+    #     relations = SuiteCaseRelation.objects.filter(
+    #         suite=suite,
+    #         case_id__in=case_ids
+    #     )
+    #
+    #     # 执行删除操作
+    #     deleted_count, _ = relations.delete()
+    #
+    #     # 构建响应
+    #     response_data = {
+    #         "success_count": deleted_count,
+    #         "failed_count": len(case_ids) - deleted_count
+    #     }
+    #     return Response(data=response_data)
+    #
+    # @action(detail=True, methods=['post'], url_path='reorder-cases')
+    # def reorder_cases(self, request, pk=None):
+    #     """调整用例顺序
+    #     request body example:
+    #         [{"case_id": 1, "order": 1}, {"case_id": 2, "order": 2}]
+    #     """
+    #     suite = self.get_object()
+    #     data = request.data
+    #     relations_to_update = []
+    #     for case_info in data:
+    #         try:
+    #             case = TestCase.objects.get(id=case_info['case_id'])
+    #             relation_record_id = SuiteCaseRelation.objects.get(suite_id=suite, case_id=case).id
+    #             relations_to_update.append(
+    #                 SuiteCaseRelation(
+    #                     id=relation_record_id,
+    #                     order=case_info['order'],
+    #                 )
+    #             )
+    #         except TestCase.DoesNotExist:
+    #             return Response({
+    #                 'code': ErrorCode.PARAM_ERROR.code,
+    #                 'message': f"用例 {case_info['case_id']} 不存在",
+    #             }, status=400)
+    #         except SuiteCaseRelation.DoesNotExist:
+    #             return Response({
+    #                 'code': ErrorCode.PARAM_ERROR.code,
+    #                 'message': f"用例 {case_info['case_id']} 不在套件 {suite.name} 中",
+    #             }, status=400)
+    #     SuiteCaseRelation.objects.bulk_update(relations_to_update, ['order'])
+    #     return Response({'status': 'success'})
 
     @action(detail=True, methods=['post'], url_path='execute')
     def execute(self, request, pk=None):
